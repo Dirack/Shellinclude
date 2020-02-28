845f1d0f0bd35ba6670cfe9518288e1d34d20b4d
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-02-28 12:53:22 -0300

    Referência ao wiki nos scripts auxiliares
    Retira a descrição das funções dos scripts auxiliares dos cabeçalhos dos
    códigos fonte e adiciona referência ao wiki no github

--- mensagemAjuda.sh
+++ mensagemAjuda.sh
@@ -5,41 +5,8 @@
 # Objetivo: Função para formatar a mensagem de ajuda dos programas
 # da biblioteca shell script.
 #
-# Fornecer os parâmetros para a função 'exibirMensagemAjudaProgramaFormatada'
-# da seguinte maneira:
-#
-#		$1 NOME DO PROGRAMA
-#		$2 DESCRIÇÃO
-#		$3 PARÂMETROS
-#		$4 EXEMPLO DE USO
-#
-# Os parâmetros devem ser passados em uma variável em que cada linha é um parâmetro,
-# separado de sua legenda explicativa por '::' como no exemplo abaixo:
-#
-# PARÂMETROS="-h --help:: Exibe esta mensagem de ajuda e sai
-# 	      -v --version:: Exibe a versão do programa e sai
-#	      -c:: Realiza alguma coisa"
-#
-# Exemplo de uso desta biblioteca:
-#
-#	~$ source mensagemAjuda.sh
-#	~$ NOME_PROGRAMA="TESTE"
-#	~$ DESCRICAO="Este é um teste"
-#	~$ PARAMETROS="-h --help::Exibe ajuda e sai"
-#	~$ EXEMPLO_DE_USO="Exemplo de teste"
-#	~$ exibirMensagemAjudaProgramaFormatada "$NOME_PROGRAMA" "$DESCRICAO" "$PARAMETROS" "$EXEMPLO_DE_USO"
-#
-#	Saída esperada:
-#
-#		NOME
-#			TESTE
-#		DESCRIÇÃO
-#			Este é um teste
-#		PARÂMETROS
-#			-h --help Exibe ajuda e sai
-#		EXEMPLO DE USO
-#			Exemplo de teste
-#
+# Para uma descrição mais detalhada desta biblioteca, consulte:
+# https://github.com/Dirack/Shellinclude/wiki/mensagemAjuda.sh
 # 
 # Site: http://www.dirackslouge.online
 #
