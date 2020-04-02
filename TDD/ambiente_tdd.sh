8ca23e647f1eac85880d07e8881e5ddc77fe5c21
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-04-02 18:11:20 -0300

    Teste do ambiente de desenvolvimento #48
    Suíte de testes do ambiente de desenvolvimento da Shellinclude.
    Este primeiro teste verifica se a variável de ambiente $PATH está setada
    corretamente para incluir a Shellinclude

---
+++ TDD/ambiente_tdd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# ambiente_tdd.sh (Shell Script)
+# 
+# Objetivo: Teste do ambiente de desenvolvimento da Shellinclude.
+# 
+# Site: https://dirack.github.io
+# 
+# Versão 1.0
+# 
+# Programador: Rodolfo A C Neves (Dirack) 02/04/2020
+# 
+# Email: rodolfo_profissional@hotmail.com
+# 
+# Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+source $(dirname $0)/tdd_lib.sh
+
+error "$(echo $PATH | grep -q /Shellinclude && echo $?)" "0" "1" "Verificar se Shellinclude faz parte do PATH"
