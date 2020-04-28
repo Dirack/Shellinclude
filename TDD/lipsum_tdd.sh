fd081160c1f8375b771dfa24bdea79bd8431e1c3
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-04-27 22:42:53 -0300

    Teste iterativo do programa lipsum #48
    Roda o comando 'lipsum -r' 1000 vezes para verificar se o programa
    retorna o número correto de linhas. Este teste foi criado para verificação
    do bug referenciado na issue #49, onde o programa exibe uma linha a menos
    que o solicitado

--- TDD/lipsum_tdd.sh
+++ TDD/lipsum_tdd.sh
@@ -19,3 +19,16 @@ source $(dirname $0)/tdd_lib.sh
 # Testar se lipsum exibe a quantidade de linhas pedidas corretamente
 error "$(lipsum -r | wc -l)" "1" "1" "Exibir uma linha com lipsum -r"
 error "$(lipsum -r 10 | wc -l)" "10" "2" "Exibir 10 linhas com lipsum -r 10"
+
+# Testar condição limite
+total="1000"
+for i in $(seq $total)
+do
+	echo -ne "Rodando lipsum repetidamente ($i/$total)\r\r"
+	LINHAS=$(lipsum -r | wc -l)
+	if [ "$LINHAS" -ne "1" ]
+	then
+		echo " "
+		error "$LINHAS" "1" "3" "Exibir uma linha com lipsum -r"
+	fi
+done
