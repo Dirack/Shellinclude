90ac48eaab39276b7828eb41841cfc4168976355
Dirack <rodolfo_profissional@hotmail.com>
2025-02-16 15:50:30 -0300

    update script

--- infra/scripts/prepare_release_msg.sh
+++ infra/scripts/prepare_release_msg.sh
@@ -6,7 +6,7 @@ CURRENTVERSION=$(git tag | sort -r | tr '\n' ' ' | cut -d" " -f1)
 
 LATESTVERSION=$(git tag | sort -r | tr '\n' ' ' | cut -d" " -f2)
 
-GITLOG=$(git log "$LATESTVERSION..$CURRENTVERSION" --oneline)
+GITLOG=$(git log "$LATESTVERSION..HEAD" --oneline)
 
 ADDED=$(echo "$GITLOG" | grep -E "\[(feat|test)\]" | sed 's/^/- /')
 if [ -n "$ADDED" ]
