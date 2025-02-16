6cf2f51e45681719f7cf8a237db5b9b784c54165
Dirack <rodolfo_profissional@hotmail.com>
2025-02-16 14:17:31 -0300

    [chore](documentation check): Check if documentation is installed

---
+++ infra/scripts/doc_install_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for doc in $(ls docs/*.1)
+do
+    man -f "$doc"
+    subject=$(echo $?)
+    man -w "$doc"
+    docfile=$(echo $?)
+	if [ "$docfile" -ne 0 -o "$subject" -ne 0 ]
+    then
+		echo "ERROR: Documentation ${doc} not found!"
+		exit 1
+	fi
+	echo "Documentation checked: ${doc} file"
+done
