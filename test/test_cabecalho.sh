1ddd27335389a0317248f586ab3a4bc116e6fa1c
Dirack <rodolfo_profissional@hotmail.com>
2025-02-16 18:07:03 -0300

    [test](cabecalho): Add unit tests suite for cabecalho

---
+++ test/test_cabecalho.sh
@@ -0,0 +1,13 @@
+source shellunity
+
+cd ../src
+
+
+LIB_VERSION=$(cat ../docs/VERSION.md)
+PROG_VERSION=$(./cabecalho -v | cut -d" " -f2)
+
+TEST_ASSERT_EQUAL_STRING "$LIB_VERSION" "$PROG_VERSION"
+
+PROG_VERSION=$(./cabecalho --version | cut -d" " -f2)
+
+TEST_ASSERT_EQUAL_STRING "$LIB_VERSION" "$PROG_VERSION"
